922c603589a9993385e9af4bb1cc10889aa6f099
atsb <44937323+atsb@users.noreply.github.com>
2025-10-10 17:50:16 +0200

    Merge pull request #511 from atsb/objFilterFix
    Fix the object filter not working
    
    (cherry picked from commit 4e5201bf041099f60a0e9c8ec3757dce0a901f97)

--- src/engine/r_drawlist.c
+++ src/engine/r_drawlist.c
@@ -38,6 +38,7 @@ static float envcolor[4] = { 0, 0, 0, 0 };
 drawlist_t drawlist[NUMDRAWLISTS];
 
 CVAR_EXTERNAL(r_texturecombiner);
+CVAR_EXTERNAL(r_filter);
 CVAR_EXTERNAL(r_objectFilter);
 
 CVAR(r_transparencynightmare, 1);
@@ -151,7 +152,6 @@ void DL_ProcessDrawList(int tag, boolean(*procfunc)(vtxlist_t*, int*)) {
     vtxlist_t* head;
     vtxlist_t* tail;
     boolean checkNightmare = false;
-    boolean obj_nearest_bypass = false;
 
     if (tag < 0 || tag >= NUMDRAWLISTS) {
         return;
@@ -179,8 +179,6 @@ void DL_ProcessDrawList(int tag, boolean(*procfunc)(vtxlist_t*, int*)) {
             if (tag != DLT_SPRITE && is_translucent_entry(tag, head)) {
                 continue;
             }
-
-            obj_nearest_bypass = false;
 
             // break if no data found in list
             if (!head->data) {
@@ -232,13 +230,8 @@ void DL_ProcessDrawList(int tag, boolean(*procfunc)(vtxlist_t*, int*)) {
 #endif
                 GL_BindSpriteTexture(head->texid, palette);
 
-                // Non-monster objects obey r_objectFilter
-                if (!(flags & MF_COUNTKILL) && ((int)r_objectFilter.value > 0)) {
-                    I_ShaderUnBind();
-                    dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-                    dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-                    obj_nearest_bypass = true;
-                }
+                dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, (int)(r_objectFilter.value == 0 && r_filter.value > 0) ? GL_LINEAR : GL_NEAREST);
+                dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, (int)(r_objectFilter.value == 0 && r_filter.value > 0) ? GL_LINEAR : GL_NEAREST);
 
                 // change blend states for nightmare things
                 if ((checkNightmare ^ (flags & MF_NIGHTMARE))) {
@@ -277,10 +270,7 @@ void DL_ProcessDrawList(int tag, boolean(*procfunc)(vtxlist_t*, int*)) {
 
             dglDrawGeometry(drawcount, drawVertex);
 
-            if (obj_nearest_bypass) {
-                I_ShaderBind();
-                obj_nearest_bypass = false;
-            }
+            I_ShaderBind();
 
             // count vertex size
             if (devparm) {
