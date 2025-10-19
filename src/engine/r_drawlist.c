1b59eb6fafa407554ca2cc19737ef9165a4e0fa5
atsb <44937323+atsb@users.noreply.github.com>
2025-10-20 01:15:16 +0200

    Fixes #520

--- src/engine/r_drawlist.c
+++ src/engine/r_drawlist.c
@@ -230,9 +230,6 @@ void DL_ProcessDrawList(int tag, boolean(*procfunc)(vtxlist_t*, int*)) {
 #endif
                 GL_BindSpriteTexture(head->texid, palette);
 
-                dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, (int)(r_objectFilter.value == 0 && r_filter.value > 0) ? GL_LINEAR : GL_NEAREST);
-                dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, (int)(r_objectFilter.value == 0 && r_filter.value > 0) ? GL_LINEAR : GL_NEAREST);
-
                 // change blend states for nightmare things
                 if ((checkNightmare ^ (flags & MF_NIGHTMARE))) {
                     if (!checkNightmare && (flags & MF_NIGHTMARE)) {
