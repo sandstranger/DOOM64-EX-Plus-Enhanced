261849da101feddb0e42f48ae97d67ab460a9bba
sandstranger <sandstranger@gmail.com>
2026-06-04 21:08:24 +0400

    feat: vk shader cache was implemented

--- src/engine/i_video.c
+++ src/engine/i_video.c
@@ -25,6 +25,7 @@
 #include <SDL3/SDL_opengl.h>
 #else
 #include "glad.h"
+#include "AngleShaderCache.h"
 #endif
 
 #ifdef SDL_PLATFORM_WIN32
@@ -391,6 +392,7 @@ void I_InitScreen(void) {
     SDL_GL_MakeCurrent(window, glContext);
 
 #if ANDROID
+    angle_blobcache_install("doom64_ex_plus_enhanced");
     if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
         SDL_GL_DestroyContext(glContext);
         SDL_DestroyWindow(window);
